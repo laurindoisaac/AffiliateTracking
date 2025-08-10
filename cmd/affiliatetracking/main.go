1f9f09b5136ce57547987b102d0e4d5d3a5d85c5
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 08:37:55 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/affiliatetracking/main.go
@@ -0,0 +1,23 @@
+// cmd/affiliatetracking/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "affiliatetracking/internal/affiliatetracking"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := affiliatetracking.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
